4e8e08b321da05863718ce970c2fc26b8c836e73
ADMDM <128033722+ADMDM@users.noreply.github.com>
2023-03-18 21:36:39 +0100

    Update PJ1.py

--- PJ1.py
+++ PJ1.py
@@ -5,7 +5,7 @@ Created on Thu Mar  2 17:11:04 2023
 
 @author: adamdavidmalila
 
-   #Projet groupe 11 :Script partie Informatique
+    #Projet groupe 11 :Script partie Informatique
 # Importation des packages nécessaires
 import numpy as np
 import pandas as pd
@@ -72,21 +72,31 @@ print(stats)
 # Chargement du dataframe data depuis le fichier CSV
 data = pd.read_csv('data_LBIR1271.csv', parse_dates=['Date'], index_col='Date')
 
+# Convertir les valeurs de H45E et H45T de g/m³ à %
+data['H45E'] = data['H45E'] * 0.1
+data['H45T'] = data['H45T'] * 0.1
 
 # Figure 1: Rain E, Rain T, H45E et H45T
 
 # Création d'une figure contenant 4 sous-graphiques
-
 fig, axs = plt.subplots(4, 1, figsize=(10, 10))
 
-axs[0].plot(data.index, data['Rain_E']) 
+# Plot de Rain_E (mm)
+axs[0].plot(data.index, data['Rain_E'])
 axs[0].set_title('Rain_E (mm)')
+
+# Plot de Rain_T (mm)
 axs[1].plot(data.index, data['Rain_T'])
 axs[1].set_title('Rain_T (mm)')
-axs[2].plot(data.index, data['H45E'])
-axs[2].set_title('H45E (g/m³)')
-axs[3].plot(data.index, data['H45T'])
-axs[3].set_title('H45T (g/m³)')
+
+# Plot de H45E (%)
+axs[2].plot(data.index, data['H45E'].dropna(), 'o-',)
+axs[2].set_title('H45E (%)')
+
+# Plot de H45T (%)
+axs[3].plot(data.index, data['H45T'].dropna(), 'o-')
+axs[3].set_title('H45T (%)')
+
 fig.suptitle('Figure 1: Rain_E, Rain_T, H45E, and H45T')
 fig.tight_layout(pad=3.0, h_pad=1.5)
 plt.show()
@@ -95,54 +105,76 @@ plt.show()
 # Figure 2
 fig, axs = plt.subplots(6, 1, figsize=(10, 15))
 
-axs[0].plot(data.index, data['Irradiance']) 
+# Plot de Irradiance (kWh/m²)
+axs[0].plot(data.index, data['Irradiance'])
 axs[0].set_title('Irradiance (kWh/m²)')
+
+# Plot de Rain (mm)
 axs[1].plot(data.index, data['Rain'])
 axs[1].set_title('Rain (mm)')
+
+# Plot de Temp (°C)
 axs[2].plot(data.index, data['Temp'])
 axs[2].set_title('Temp (°C)')
+
+# Plot de Wind (m/s)
 axs[3].plot(data.index, data['Wind'])
 axs[3].set_title('Wind (m/s)')
+
+# Plot de Rel_hum (%)
 axs[4].plot(data.index, data['Rel_hum'])
 axs[4].set_title('Rel_hum (%)')
+
+# Plot de SD (m)
 axs[5].plot(data.index, data['SD'])
 axs[5].set_title('SD (m)')
+
 fig.suptitle('Figure 2: Irradiance, Rain, Temp, Wind, Rel_hum and SD')
 fig.tight_layout(pad=3.0, h_pad=1.5)
 plt.show()
 
 
-
 # Figure 3
 fig, axs = plt.subplots(6, 1, figsize=(10, 15))
 
+# Ajout du premier sous-graphique : E153 (g/s)
 axs[0].plot(data.index, data['E153']) 
 axs[0].set_title('E153 (g/s)')
+
+# Ajout du deuxième sous-graphique : E159 (g/s)
 axs[1].plot(data.index, data['E159'])
 axs[1].set_title('E159 (g/s)')
+
+# Ajout du troisième sous-graphique : E161 (g/s)
 axs[2].plot(data.index, data['E161'])
 axs[2].set_title('E161 (g/s)')
+
+# Ajout du quatrième sous-graphique : T13 (°C)
 axs[3].plot(data.index, data['T13'])
 axs[3].set_title('T13 (°C)')
+
+# Ajout du cinquième sous-graphique : T21 (°C)
 axs[4].plot(data.index, data['T21'])
 axs[4].set_title('T21 (°C)')
+
+# Ajout du sixième sous-graphique : T22 (°C)
 axs[5].plot(data.index, data['T22'])
 axs[5].set_title('T22 (°C)')
+
+# Ajout d'un titre à la figure
 fig.suptitle('Figure 3: E153, E159, E161, T13, T21 and T22')
+
+# Ajustement de la disposition des sous-graphiques
 fig.tight_layout(pad=3.0, h_pad=1.5)
+
+# Affichage de la figure
 plt.show()
 
-plt.savefig('figure3.png')
-#Sauvergarder 
-plt.savefig('figure1.png')
-plt.savefig('figure2.png')
+# Sauvegarde de la figure
 plt.savefig('figure3.png')
 
 
-# Ajout du premier sous-graphique : Rain_E (mm)
-axs[0].plot
 
-#%%
 # Calculer la matrice de corrélation
 corr_matrix = data.corr()
 
@@ -153,7 +185,7 @@ plt.title('Matrice de corrélation des variables météorologiques')
 plt.savefig('heatmap.png', dpi=300, bbox_inches='tight')
 plt.show()
 
-#%%
+
 
 # Sélection des variables à inclure dans la figure
 variables = ["Temp", "Rain", "Wind", "Irradiance", "Rel_hum", "SD", "Rain_E", "H45E", "E153"]
